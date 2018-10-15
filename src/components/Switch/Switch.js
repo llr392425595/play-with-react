272de6d594375e9bfc0f251d7dd580881b5d0053
Liang <392425595@qq.com>
2018-10-16 00:33:29 +0800

    add Switch component to show event handler

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,5 +1,25 @@
 import React from 'react'
 
-export default () => (
-    <div>Switch</div>
-)
+class Switch extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+    }
+  
+    handleClick() {
+      this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
+    }
+  
+    render() {
+      return (
+        // 调用bind方法绑定时间到this指向的对象
+        <button onClick={this.handleClick.bind(this)}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
+  }
+
+  export default Switch

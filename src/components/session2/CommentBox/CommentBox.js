ac13a38f6d2aed58f18f33b6b8fe6766c3af8625
Liang <392425595@qq.com>
2018-10-21 14:12:07 +0800

    update post page with styled-components

--- src/components/session2/CommentBox/CommentBox.js
+++ src/components/session2/CommentBox/CommentBox.js
@@ -2,10 +2,61 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 class CommentBox extends Component {
+  state = {
+    text : '',
+    comments: [{
+        id:'0',
+        text:'hello'
+      },{
+        id:'1',
+        text:'hah'
+      }]
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  submitCmt = (e) => {
+    e.preventDefault()
+    const { text } = this.state
+    const id = this.state.comments.length
+    const comments = [
+      ...this.state.comments,
+      { id, text }
+    ]
+    this.setState({
+      text: '',
+      comments
+    })
+  }
+
   render () {
+    const cmtForm = (
+      <FormWrap>
+        <Input 
+          value={this.state.text} 
+          onChange={this.handleChange}
+        />
+        <Button onClick={this.submitCmt}>提交</Button>
+      </FormWrap>
+    )
+
+    const { comments } = this.state
+    const reversedComments = [...comments].reverse()
+    const cmtList = reversedComments.map(
+      t => <div key={t.id}>{t.text}</div>
+    )
+
     return (
       <Wrap>
-        Comments
+        {cmtForm}
+        <CmtList>
+          {cmtList}
+        </CmtList>
       </Wrap>
     )
   }
@@ -14,9 +65,44 @@ class CommentBox extends Component {
 export default CommentBox
 
 const Wrap = styled.div`
-  background-color: #fff;
   width: 400px;
   margin: 30px auto;
   box-shadow: 0 2px 2px rgba(0, 0, 0, .5);
   height: 300px;
+  min-height: 300px;
+`
+const FormWrap = styled.div`
+  display: flex;
+  padding: 10px;
+`
+const Input = styled.input`
+  flex-grow: 1;
+  height: 30px;
+  line-height: 30px;
+  border: 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  :focus {
+    outline: none;
+    border-bottom: 1px solid deeppink;
+  }
+`
+const Button = styled.div`
+  background-color: deeppink;
+  margin-left: 10px;
+  padding: 0 10px;
+  border: none;
+  line-height: 30px;
+  :focus {
+    outline: none;
+  }
+  :hover {
+    cursor: pointer;
+  }
+  color: white;
+`
+const CmtList = styled.div`
+  padding: 10px;
+  div {
+    line-height: 30px;
+  }
 `

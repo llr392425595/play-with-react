6eb430be4f172586c66f6088a1367b5b404895da
Liang <392425595@qq.com>
2018-11-04 21:34:12 +0800

    调整session2视图结构

--- src/components/Main/Session2.js
+++ src/components/Main/Session2.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
-import {Route} from 'react-router-dom'
+import {Link, Route} from 'react-router-dom'
 import Home from '../session2/Home/Home'
 import Post from '../session2/Post/Post'
 
 class Session2 extends Component {
   render() {
     return (
-      <div className="content">
-        <Route exact path="/session2/" component={Home}/>
-        <Route path="/session2/post/:id" component={Post}/>
+      <div>
+        <div className="header">
+          <div><Link to={`/session2/home`}>Post-Detail-Comment案例</Link></div>
+        </div>
+        <div className="content">
+          <Route exact path="/session2/home" component={Home}/>
+          <Route path="/session2/post/:id" component={Post}/>
+        </div>
       </div>
     )
   }
